19018a9fe20e7d860618a954629188366e8f07f2
Kiryl Dorakh <33548431+KirillDorokh@users.noreply.github.com>
2020-12-22 23:41:58 +0400

    Update Volunteers.py

--- C1.10.3-4_pet's_house/Volunteers.py
+++ C1.10.3-4_pet's_house/Volunteers.py
@@ -1,12 +1,15 @@
-from house_for_pets import Volunteer
+from house_for_pets import Volunteer, GuestList
 
 vasiliy_nikolaev = Volunteer('Василий Николаев', 'Москва', 'Наставник')
 nikolay_sobolev = Volunteer('Николай Соболев', 'Питер', 'Спикер')
 aleksey_kvashonkin = Volunteer('Алексей Квашонкин', 'Киев', 'Ученик')
 alex_andreev = Volunteer('Алексей Андреев', 'Львов', 'Мастер')
 
+list_of_guest = GuestList()
+
 corporate_list = [vasiliy_nikolaev, nikolay_sobolev, aleksey_kvashonkin, alex_andreev]
 
-print('\nСписок для корпоратива:')
 for volonter in corporate_list:
-    print(volonter.info())
+    list_of_guest.getGuest(volonter)
+    
+print(list_of_guest.getGuestList())
