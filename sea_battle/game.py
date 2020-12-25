cf18c1afe841888f1526e05c8ae8a57c81100abe
Kiryl Dorakh <33548431+KirillDorokh@users.noreply.github.com>
2020-12-25 23:21:59 +0400

    Update game.py

--- sea_battle/game.py
+++ sea_battle/game.py
@@ -1,66 +1,151 @@
-from sea_battle_classes import Game, Ship, Player, AI, Field, History
-
+from Class import Game, AiShip, Field, PlayerShip
+# Приветствие
 Game.hello()
-new_game = Game()
-test_field = Field()
-player = Player()
-ai = AI()
-player_history = History()
-ai_history = History()
 
-# Ввод координат для трехпалубного
-coords7 = player.getCoordsForThree()
-three1 = Ship(coords7)
-test_field.cheakInput(coords7)
-print("Поле для расстановки")
-test_field.test()
-print("---------------------------")
+game = Game()
+ai_secret_field = Field()
+ai_field = Field()
+player_field = Field()
+ai = AiShip()
+player = PlayerShip()
 
-# Ввод первого двухпалубного
-coords5 = player.getCoordsForTwo()
-two1 = Ship(coords5)
-test_field.cheakInput(coords5)
-print("Поле для расстановки")
-test_field.test()
-print("---------------------------")
+Game.pause()
+print("-----------------------------------------")
+print('               Старт игры')
 
-# Ввод второго двухпалубного
-coords6 = player.getCoordsForTwo()
-two2 = Ship(coords6)
-test_field.cheakInput(coords6)
-print("Поле для расстановки")
-test_field.test()
-print("---------------------------")
+# Расстановка кораблей
+def input_field():
+    print("-----------------------------------------")
+    print('ИИ расставляет корабли...')
+    print("-----------------------------------------")
+    ai_ships = ai.ships()
 
-# Ввод первого однопалубного
-coords1 = player.getCooordsForOne()
-one1 = Ship(coords1)
-test_field.cheakInput(coords1)
-print("Поле для расстановки")
-test_field.test()
-print("---------------------------")
+    for i in ai_ships:
+        ai_secret_field.cheakInput(i)
 
-# Ввод второго однопалубного
-coords2 = player.getCooordsForOne()
-one2 = Ship(coords2)
-test_field.cheakInput(coords2)
-print("Поле для расстановки")
-test_field.test()
-print("---------------------------")
-# Ввод третьего однопалубного
-coords3 = player.getCooordsForOne()
-one3 = Ship(coords3)
-test_field.cheakInput(coords3)
-print("Поле для расстановки")
-test_field.test()
-print("---------------------------")
+    #print("-----------------------------------------")
+    #print("               Поле ИИ")
+    #ai_secret_field.test()
 
-# Ввод четвертого однопалубного
-coords4 = player.getCooordsForOne()
-one4 = Ship(coords4)
-test_field.cheakInput(coords4)
-print("Поле для расстановки")
-test_field.test()
-print("---------------------------")
+    player_ships = player.ships()
+    print('Координаты кораблей Игрока:', player_ships)
+
+    for i in player_ships:
+        player_field.cheakInput(i)
+    print("-----------------------------------------")
+    print("        Ввод координат завершен")
+    print("-----------------------------------------")
+    print("              Поле Игрока")
+    player_field.test()
+    print("                Поле ИИ")
+    ai_field.test()
+    print("-----------------------------------------")
+    print("          Начинаем стрелять")
+
+# Проверка стрельбы с прошлыми выстрелами
+def ai_coorect_shoot(shoot):
+    n = ai.getShootHistory().count(shoot)
+    if n == 0:
+        print('Выстрел разрешен')
+        return shoot
+    else:
+        shoot = game.aiShot()
+        return ai_coorect_shoot(shoot)
+
+# Проверка стрельбы с прошлыми выстрелами
+def player_coorect_shoot(shoot):
+    n = player.getShootHistory().count(shoot)
+    if n == 0:
+        print('Выстрел разрешен')
+        return shoot
+    else:
+        print(f'{shoot} уже обстрелян!')
+        shoot = game.playerShot()
+        return player_coorect_shoot(shoot)
+
+# Проверка попадания на пробитие
+def second_chance_for_player(cheak):
+    if cheak == "Попадание!!!":
+        ai_field.test()
+        print(f'Ходите еще раз')
+        shoot = game.playerShot()
+        shoot4 = player_coorect_shoot(shoot)
+        player.writeShootHistory(shoot4)
+        print("Player's Shoots", player.getShootHistory())
+        cheak = ai_secret_field.cheakShootForAIField(shoot4)
+        cheak1 = ai_field.cheakFromSecretAIField(*cheak)
+        return second_chance_for_player(cheak1)
+    else:
+        return "Переход хода"
+
+def second_chance_for_ai(cheak):
+    if cheak == "Попадание!!!":
+        print(f'AI ходит еще раз')
+        player_field.test()
+        shoot = game.aiShot()
+        shoot1 = ai_coorect_shoot(shoot)
+
+        ai.writeShootHistory(shoot1)
+        print("AI's Shoots", ai.getShootHistory())
+        cheak1 = player_field.cheakShoot(shoot1)
+        return second_chance_for_player(cheak1)
+    else:
+        return print("Переход хода")
+
+def cheak_results(ai_result, player_result):
+    if ai_result:
+        print("-----------------------------------------")
+        return ai_result
+    if player_result:
+        print("-----------------------------------------")
+        return player_result
 
 
+# Основная игра
+def main_play():
+    result = None
+    while not result:
+        print("-----------------------------------------")
+        print('             Поле Игрока')
+
+        shoot = next(game.changeOfCourse())
+        shoot1 = ai_coorect_shoot(shoot)
+
+        ai.writeShootHistory(shoot1)
+        print("AI's Shoots", ai.getShootHistory())
+        cheak2 = player_field.cheakShoot(shoot1)
+        player_field.test()
+        second_chance_for_ai(cheak2)
+        ai_result = player_field.cheak_result_player()
+
+        # Ход игрока
+        print("-----------------------------------------")
+        print('              Поле ИИ')
+        ai_field.test()
+        shoot3 = next(game.changeOfCourse())
+        shoot4 = player_coorect_shoot(shoot3)
+
+        player.writeShootHistory(shoot4)
+        print("Player's Shoots", player.getShootHistory())
+        cheak = ai_secret_field.cheakShootForAIField(shoot4)
+        cheak1 = ai_field.cheakFromSecretAIField(*cheak)
+        print("-----------------------------------------")
+        print('              Поле ИИ')
+        ai_field.test()
+        second_chance_for_player(cheak1)
+
+        #проверка результата
+        player_result = ai_secret_field.cheak_result_player()
+        result = cheak_results(ai_result, player_result)
+    return result
+
+
+
+input_field()
+
+Game.pause()
+
+print(f'Победил {main_play()} !!!!')
+print('Конец игры')
+
+Game.pause()
